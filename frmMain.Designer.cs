ef50a2fe29ccf78cdf0082a567f46d870561c2eb
Manlx <74509303+Manlx@users.noreply.github.com>
2021-08-31 23:40:56 +0200

    More Commits

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -3,15 +3,7 @@ namespace JavaCompilerBatGenerator
 {
     partial class frmMain
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
